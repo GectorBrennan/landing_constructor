793824b4db4b201bf969d90f544def6c9c34229e
ErnestAmconsoftern <php.amconsoftern@gmail.com>
2019-10-26 19:16:21 +0300

    rm

--- install_app.sh
+++ install_app.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 cp .env.example .env
-cd docker
-cp .env.example .env
 docker stop $(docker ps -sq)
-docker-compose up -d nginx mysql
-docker-compose exec workspace composer install
-docker-compose exec workspace yarn install
-docker-compose exec workspace yarn run prod
-docker-compose exec workspace php artisan migrate
-docker-compose exec workspace php artisan storage:link
+docker-compose up -d
+docker-compose exec app composer install
+docker-compose exec app npm install
+docker-compose exec app npm run prod
+docker-compose exec app php artisan migrate
--- readme.md
+++ readme.md
@@ -1,18 +1,6 @@
-#1 Requirements
-1. Docker 1.13.0+
-2. Docker-Compose
-
-## 2. Installation
+## Install app
 1. Run installation script:<br />
 `sh install_app.sh`
 
 2. Add host to /etc/hosts:<br />
-`127.0.0.1 test-task.local`
-
-
-## 3. Technologies
-1. Docker
-2. PHP 7.2
-3. MySql (latest)
-4. Laravel Framework 5.7
-5. Vue.js
+`127.0.0.1 landing.docker`
